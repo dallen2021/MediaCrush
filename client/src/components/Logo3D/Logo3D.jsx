3e23c68fc2c70dd65dae0d079495fcf4440019a2
dallen2021 <danielballen2021@gmail.com>
2026-03-24 08:18:38 -0700

    Boost 3D logo lighting so gray parts are actually visible

--- client/src/components/Logo3D/Logo3D.jsx
+++ client/src/components/Logo3D/Logo3D.jsx
@@ -12,8 +12,9 @@ export default function Logo3D() {
         gl={{ antialias: true, alpha: true }}
         dpr={[1, 2]}
       >
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 8, 5]} intensity={1} />
+        <ambientLight intensity={1.8} />
+        <directionalLight position={[5, 8, 5]} intensity={2.5} />
+        <directionalLight position={[-3, 2, 4]} intensity={1.2} />
         <Suspense fallback={null}>
           <PressScene />
         </Suspense>
